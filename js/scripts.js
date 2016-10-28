de365022187a18a34c28fc9f43f45a53531fb54c
Brad Copenhaver <bradcopenhaver@gmail.com>
2016-10-28 14:06:26 -0700

    make one js variable global to avoid repetitive hide calls, add easter egg output

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
+var outputPane = "#pregame";
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+
+    $(outputPane).hide();
 
     var inputLimit = parseInt($("#inputLimit").val());
     var inputPing = parseInt($("#inputPing").val());
     var inputPong = parseInt($("#inputPong").val());
 
     var outputString = createList(inputLimit, inputPing, inputPong);
-    var outputPane = easterEgg(inputLimit);
+    outputPane = easterEgg(inputLimit);
 
     $("#outputList").text(outputString);
-    $("#pregame").hide();
     $(outputPane).show();
   });
 });
@@ -21,7 +24,10 @@ var createList = function(inputLimit, inputPing, inputPong) {
     return "A game to zero is not a game at all! Try playing to a higher number."
   }
   else if (inputLimit < 0) {
-    return "You can't play to a negative number! Try a positive number."
+    return "Oops! You can't play to a negative number! Try a positive number."
+  }
+  else if (inputLimit > 100000) {
+    return "Woah! That game would take forever! Try a smaller number."
   }
   else {
     var numberArray = [];
@@ -55,6 +61,9 @@ var easterEgg = function(input) {
   }
   if (input === -117){
     return "#halo";
+  }
+  if (input === -81382){
+    return "#birthday";
   }
   else {
     return "#output";
