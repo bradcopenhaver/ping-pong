0fc526beda98c8479adf2cbca4d4e1499661b8d8
Brad Copenhaver <bradcopenhaver@gmail.com>
2016-10-28 10:21:58 -0700

    add content and styling to interface. hide/show output field

--- js/scripts.js
+++ js/scripts.js
@@ -9,35 +9,30 @@ $(document).ready(function(){
     var outputList = createList(inputLimit, inputPing, inputPong);
 
     $("#outputList").text(outputList);
-
+    $("#output").show();
   });
 });
-
 var createList = function(inputLimit, inputPing, inputPong) {
   var numberArray = [];
-
-//Populate the array.
+  //Populate the array.
   for (var i=0; i<inputLimit+1; i++) {
     numberArray.push(i);
   }
-
-//Clear ping and pong multiples.
+  //Clear ping and pong multiples.
   for (var i=inputPing; i<numberArray.length; i+=inputPing){
     numberArray[i] = "";
   }
   for (var i = inputPong; i<numberArray.length; i+=inputPong){
     numberArray[i] = "";
   }
-
-//Populate pings and pongs.
+  //Populate pings and pongs.
   for (var i=inputPing; i<numberArray.length; i+=inputPing){
     numberArray[i] += "ping";
   }
   for (var i=inputPong; i<numberArray.length; i+=inputPong){
     numberArray[i] += "pong";
   }
-
-//Remove zero and return string.
+  //Remove zero and return string.
   numberArray.shift();
   return numberArray.join(", ");
 }
